d80a12028769f25df60128a7b46a4d024eb27103
Albert Lee <leesga8@gmail.com>
2021-10-22 16:13:35 -0700

    add business logic for total points, and checked values for UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
+// Business logic:
+let swiftlang = 0;
+let pythonlang= 0;
+let jslang = 0;
+
+// value=swift, python, javascript
+// add +1 every time a radio box is checked. 
+function plusOne(x) {
+  if (){}
+  else if (){}
+}
+
+//compare with total points and return largest
+
+function totalPoints(swiftlang, pythonlang, jslang) {
+  if (swiftlang > pythonglang && swiftlang > jslang) {
+    return "You should learn Swift."
+  } else if (pythonlang > swiftlang && pythonlang > jslang) {
+    return "You should learn Python."
+  } else if (jslang > swiftlang && jslang > pythonlang) {
+    return "You should learn Javascript."
+  }
 
 
 
 
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $('#survey').submit(function(event) {
+    event.preventDefault();
+
+    $("input:radio[name=animal]:checked").val();
+    $("input:radio[name=place]:checked").val();
+    $("input:radio[name=mile]:checked").val();
+    $("input:radio[name=fruit]:checked").val();
+    $("input:radio[name=band]:checked").val();
+
+
+  });
+});
 
 
 
-
-
-
-
-
-
-
-$("input:radio[name=animal]:checked").val();
-$("input:radio[name=place]:checked").val();
-$("input:radio[name=mile]:checked").val();
-$("input:radio[name=fruit]:checked").val();
-$("input:radio[name=band]:checked").val();
+// page 41, 47,49 
